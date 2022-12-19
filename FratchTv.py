cd89822dd63573655efe3c12b878fda4b87c53b4
Fratch <57953411+Fratch@users.noreply.github.com>
2022-12-19 18:20:49 +0100

    Update FratchTv.py
    refactoring

--- FratchTv.py
+++ FratchTv.py
@@ -5,77 +5,94 @@ from datetime import date
 import vlc
 import time
 
+# Set the extensions for video files
+VIDEO_EXTENSIONS = [".mp4", ".avi", ".mkv", ".webm"]
+
 # Set the directory where the video files are located
-video_dir = "videos"
+DEFAULT_VIDEO_DIR = "videos"
 
 # Set the date ranges and corresponding directories for additional videos
-date_ranges = {
+DATE_RANGES = {
     (date(2022, 12, 1), date(2022, 12, 31)): "videos/natale",
     (date(2022, 6, 1), date(2022, 8, 31)): "videos/estate",
     (date(2022, 2, 14), date(2022, 2, 14)): "videos/sanvalentino",
 }
 
 # Set the hour ranges and corresponding directories for additional videos
-hour_ranges = {
+HOUR_RANGES = {
     (6,18): "videos/giorno",
     (18,6): "videos/notte",
 }
 
-# Create a VLC player
-media_player = vlc.MediaListPlayer()
+def get_video_files(directory):
+    """Return a list of video files in the given directory."""
+    return [f for f in os.listdir(directory) if f.endswith(tuple(VIDEO_EXTENSIONS))]
 
-# Create Instance class object
-player = vlc.Instance()
+def get_media_list(directory):
+    """Return a VLC media list for the video files in the given directory."""
+    media_list = vlc.MediaList()
+    for f in get_video_files(directory):
+        media_list.add_media(os.path.join(directory, f))
+    return media_list
 
-# Create a media list object
-media_list = vlc.MediaList()
+def add_files_to_media_list(media_list, directory):
+    """Add the video files in the given directory to the given media list."""
+    for f in get_video_files(directory):
+        media_list.add_media(os.path.join(directory, f))
 
-# Get a list of all video files in the directory
-video_files = [f for f in os.listdir(video_dir) if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mkv") or f.endswith(".webm")]
+def main():
+    try:
+        # Create a VLC player
+        media_player = vlc.MediaListPlayer()
 
-# Shuffle the list of video files
-random.shuffle(video_files)
+        # Create a media list object
+        media_list = vlc.MediaList()
 
-# Add the files to the VLC player's queue
-for f in video_files:
-    media_list.add_media(os.path.join(video_dir, f))
+        # Get a list of video files in the default directory
+        video_files = get_video_files(DEFAULT_VIDEO_DIR)
 
-# Get the current date
-today = date.today()
+        # Shuffle the list of video files
+        random.shuffle(video_files)
 
-# Get the current hour
-now = time.localtime().tm_hour
+        # Add the shuffled video files to the media list
+        for f in video_files:
+            media_list.add_media(os.path.join(DEFAULT_VIDEO_DIR, f))
 
-# Check if the current date is within any of the specified date ranges
-for date_range in date_ranges:
-    start, end = date_range
-    if today.month >= start.month and today.month <= end.month and today.day >= start.day and today.day <= end.day:
-        # If the current date is within the date range, add the video files from the corresponding folder
-        folder_path = date_ranges[date_range]
-        files = [f for f in os.listdir(folder_path) if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mkv") or f.endswith(".webm")]
-        for f in files:
-            media_list.add_media(os.path.join(folder_path, f))
+        # Get the current date and hour
+        today = date.today()
+        now = time.localtime().tm_hour
 
-# Check if the current hour is within any of the specified hour ranges
-for hour_range in hour_ranges:
-    start, end = hour_range
-    if start <= now <= end:
-        # If the current hour is within the date range, add the video files from the corresponding folder
-        folder_path = hour_ranges[hour_range]
-        files = [f for f in os.listdir(folder_path) if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mkv") or f.endswith(".webm")]
-        for f in files:
-            media_list.add_media(os.path.join(folder_path, f))
-        pass
+        # Check if the current date is within any of the specified date ranges
+        for date_range in DATE_RANGES:
+            start, end = date_range
+            if start.month <= today.month <= end.month and start.day <= today.day <= end.day:
+                # If the current date is within the date range, add the video files from the corresponding folder
+                folder_path = DATE_RANGES[date_range]
+                add_files_to_media_list(media_list, folder_path)
 
-# setting media list to the media player
-media_player.set_media_list(media_list)
+        # Check if the current hour is within any of the specified hour ranges
+        for hour_range in HOUR_RANGES:
+            start, end = hour_range
+            if start <= now <= end:
+                # If the current hour is within the date range, add the video files from the corresponding folder
+                folder_path = HOUR_RANGES[hour_range]
+                add_files_to_media_list(media_list, folder_path)
 
-# Play the video files
-media_player.play()
-while True:
-    # Wait for the user to press a key to skip to the next video
-    input("Press enter to skip to the next video in the playlist...")
+        # Set the media list to the media player
+        media_player.set_media_list(media_list)
 
-    # Skip to the next video in the playlist
-    media_player.next()
-    pass
+        # Play the video files
+        media_player.play()
+        while True:
+            # Wait for the user to press a key to skip to the next video
+            input("Press enter to skip to the next video in the playlist...\n")
+
+            # Skip to the next video in the playlist
+            media_player.next()
+        
+    except Exception as e:
+        # Catch any exceptions that occur and print an error message
+        print("An error occurred:", str(e))
+    
+if __name__ == "__main__":
+    main()
