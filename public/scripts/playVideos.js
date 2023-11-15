ffcde58a55f89b0bf40aa1922b8f8a3d1aec8c4f
Fratch <57953411+Fratch@users.noreply.github.com>
2023-11-15 09:00:52 +0100

    Update playVideos.js

--- public/scripts/playVideos.js
+++ public/scripts/playVideos.js
@@ -5,63 +5,74 @@ let shuffledVideos;
 let currentIndex = 0;
 
 playButton.addEventListener('click', () => {
-    fetch('/videos')
-        .then(response => response.json())
-        .then(videos => {
-            shuffledVideos = shuffleArray(videos);
-            playVideosRandomly();
-        });
+  fetch('/videos')
+    .then(response => response.json())
+    .then(videos => {
+      shuffledVideos = shuffleArray(videos);
+      playVideosRandomly();
+    });
 });
 
 function playVideosRandomly() {
-    // Initialize the videoPlayer instance
-    videoPlayer = videojs('videoPlayer', { fluid: true });
+  // Initialize the videoPlayer instance
+  videoPlayer = videojs('videoPlayer', { fluid: true });
 
-    // Add 'ended' event listener to the player instance
-    videoPlayer.on('ended', playNextVideo);
+  // Add 'ended' event listener to the player instance
+  videoPlayer.on('ended', playNextVideo);
 
-    // Add keyboard controls
-    document.addEventListener('keydown', handleKeyDown);
+  // Add keyboard controls
+  document.addEventListener('keydown', handleKeyDown);
 
-    // Start playing the first video
-    playNextVideo();
+  // Start playing the first video
+  playNextVideo();
 }
 
 function playNextVideo() {
-    const currentVideo = shuffledVideos[currentIndex];
-    videoPlayer.src({ src: `/video/${encodeURIComponent(currentVideo)}`, type: 'video/mp4' });
-    videoPlayer.play();
-    currentIndex = (currentIndex + 1) % shuffledVideos.length;
+  const currentVideo = shuffledVideos[currentIndex];
+  videoPlayer.src({ src: `/video/${encodeURIComponent(currentVideo)}`, type: 'video/mp4' });
+  videoPlayer.play();
+  currentIndex = (currentIndex + 1) % shuffledVideos.length;
 }
 
 function handleKeyDown(event) {
-    switch (event.key) {
-        case ' ':
-            // Space key: toggle play/pause
-            videoPlayer.paused() ? videoPlayer.play() : videoPlayer.pause();
-            break;
-        case 'ArrowRight':
-            // Right arrow key: play the next video
-            playNextVideo();
-            break;
-        case 'ArrowLeft':
-            // Left arrow key: play the previous video (if available)
-            currentIndex = (currentIndex - 2 + shuffledVideos.length) % shuffledVideos.length;
-            playNextVideo();
-            break;
-        default:
-            break;
-    }
+  switch (event.key) {
+    case ' ':
+      // Space key: toggle play/pause
+      videoPlayer.paused() ? videoPlayer.play() : videoPlayer.pause();
+      break;
+    case 'ArrowRight':
+      // Right arrow key: play the next video
+      playNextVideo();
+      break;
+    case 'ArrowLeft':
+      // Left arrow key: play the previous video (if available)
+      currentIndex = (currentIndex - 2 + shuffledVideos.length) % shuffledVideos.length;
+      playNextVideo();
+      break;
+    case 'f':
+      // 'f' key: toggle fullscreen
+      if (document.fullscreenElement) {
+        // If already in fullscreen, exit fullscreen
+        document.exitFullscreen();
+      } else {
+        // If not in fullscreen, request fullscreen
+        videoPlayer.requestFullscreen();
+      }
+      break;
+    default:
+      break;
+  }
 }
 
+
 function shuffleArray(array) {
-    let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
 
-    return array;
+  return array;
 }
